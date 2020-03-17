42cf750c72f617e73905c2256e8f9068d70228bd
hanbinleejoy <lhb1025@gmail.com>
2020-03-18 01:45:06 +0900

    200318 log4jdbc update

--- src/test/java/com/beanie/persistance/TimeMapperTests.java
+++ src/test/java/com/beanie/persistance/TimeMapperTests.java
@@ -23,8 +23,15 @@ public class TimeMapperTests {
 	
 	@Test
 	public void testGetTime() {
+		log.info("getTime");
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
 	}
 	
 
